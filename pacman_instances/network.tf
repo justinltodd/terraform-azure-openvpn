98ca73a7a352727f3399ff01b7388928009e1536
justinltodd <justinltodd@gmail.com>
2019-11-06 21:32:58 +0000

    fixed typos

--- pacman_instances/network.tf
+++ pacman_instances/network.tf
@@ -45,7 +45,7 @@ resource "azurerm_network_security_rule" "ssh" {
   destination_address_prefix  = "*"
 }
 
-# NOTE: this allows VPN from Intenet
+# NOTE: this allows VPN from Internet
 resource "azurerm_network_security_rule" "openvpn" {
   name                        = "PermitOpenVPNInbound"
   resource_group_name         = "${azurerm_resource_group.main.name}"
