65390946152f3d1747f15178170b61c307483530
justinltodd <justinltodd@gmail.com>
2019-11-07 22:41:50 +0000

    adjust file/directory structure

--- de_infrastructure/outputs.tf
+++
@@ -1,2 +0,0 @@
-output "public_ip" {
-  value = "The VPN Public IP Address: ${azurerm_public_ip.PublicIP.ip_address}"
--- de_infrastructure/spoke01.tf
+++
@@ -1,2 +0,0 @@
-# Will migrate over subnet from networks.tf
-# Each individual subnet will have their own resource group
