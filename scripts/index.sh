cb5939058ecae3c1d697699b4e4a7256a4bc6589
justinltodd <justinltodd@gmail.com>
2019-11-12 07:43:27 +0000

    removed cut replaced with awk

--- scripts/index.sh
+++ scripts/index.sh
@@ -65,7 +65,7 @@ if [[ "$NUMBEROFCLIENTS" = '0' ]]; then
 else
 	while read c; do
 		if [[ $(echo $c | grep -c "^V") = '1' ]]; then
-			clientName=$(echo $c | cut -d '=' -f 2)
+            clientName=$(echo $c | cut -d '=' -f 7 | awk -F/ '{print $1}')
 			echo "<p><a href='index.sh?option=revoke&client=$clientName'>Revoke</a> <a target='_blank' href='download.sh?client=$clientName'>Download</a> $clientName</p>"
 		fi
 	done </etc/openvpn/easy-rsa/pki/index.txt
