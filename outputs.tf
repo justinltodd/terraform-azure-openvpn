2d967db457b5cabc78910ec0268c1347781af4e2
justinltodd <justinltodd@gmail.com>
2019-11-15 23:16:38 +0000

    add private ip of vpnserver output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "public_ip" {
+output "vpnserver_public_ip" {
   value = "The VPN Public IP Address: ${azurerm_public_ip.PublicIP.ip_address}"
+}
+
+output "vpnserver_private_ip" {
+  value = "The VPN Private IP Address: ${azurerm_network_interface.vpnserver_nic.private_ip_address}"
 }
