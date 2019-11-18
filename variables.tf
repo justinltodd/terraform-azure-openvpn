8058729f7d205abaf77e10dc638c8861810b8155
Justin Todd <justinltodd@gmail.com>
2019-11-18 14:29:11 -0800

    Update variables.tf
    removed unnecessary code

--- variables.tf
+++ variables.tf
@@ -257,33 +257,7 @@ variable "client-sg" {
 # [priority, direction, access, protocol, source_port_range, destination_port_range, description]"
 # All the fields are required.
 
-# Predefined rules
-variable "predefined_rules" {
-  type    = "list"
-  default = []
-}
 
-variable "custom_rules" {
-  description = "Security rules for the network security group using this format name = [priority, direction, access, protocol, source_port_range, destination_port_range, source_address_prefix, destination_address_prefix, description]"
-  type        = "list"
-  default     = []
-}
-
-# source address prefix to be applied to all rules
-variable "source_address_prefix" {
-  type    = "list"
-  default = ["*"]
-
-  # Example ["10.0.3.0/24"] or ["VirtualNetwork"]
-}
-
-# Destination address prefix to be applied to all rules
-variable "destination_address_prefix" {
-  type    = "list"
-  default = ["*"]
-
-  # Example ["10.0.3.0/32","10.0.3.128/32"] or ["VirtualNetwork"]
-}
 # ---------------------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------------------
